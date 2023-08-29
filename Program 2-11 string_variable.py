a6fda4f7ba6fede22546d6ed88131f65d0da927c
martian94 <57848767+Scandiking@users.noreply.github.com>
2023-08-29 15:29:46 +0200

    Create Program 2-11 string_variable.py

---
+++ Program 2-11 string_variable.py
@@ -0,0 +1,10 @@
+#Create variables to reference two strings. 
+first_name = 'Scandi'
+last_name = 'King'
+
+#Display the values referenced by the variables. 
+print(first_name, last_name)
+
+#Output
+#Scandi King
+#Notice the comma is not displayed in output as it is not a literal
