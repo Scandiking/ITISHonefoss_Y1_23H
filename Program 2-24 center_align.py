2182379cf5129f26af5bacd88823a6efc8edaf3f
martian94 <57848767+Scandiking@users.noreply.github.com>
2023-08-29 16:39:58 +0200

    Create Program 2-24 center_align.py

---
+++ Program 2-24 center_align.py
@@ -0,0 +1,23 @@
+#This program demonstrates how to center-align strings.
+name1 = 'Gordon'
+name2 = 'Smith'
+name3 = 'Washington'
+name4 = 'Alvarado'
+name5 = 'Livingston'
+name6 = 'Jones'
+
+#Display the names.
+print(f'***{name1:^20}***')
+print(f'***{name2:^20}***')
+print(f'***{name3:^20}***')
+print(f'***{name4:^20}***')
+print(f'***{name5:^20}***')
+print(f'***{name6:^20}***')
+
+#Output should look like:
+#***       Gordon       ***
+#***       Smith        ***
+#***     Washington     ***
+#***      Alvarado      ***
+#***     Livingston     ***
+#***       Jones        ***
