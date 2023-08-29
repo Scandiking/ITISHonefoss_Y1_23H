eb31fa7a44897a13ded30be1eaa3d2f92dd19515
Scandiking <57848767+Scandiking@users.noreply.github.com>
2023-08-29 14:49:57 +0200

    Create Program 2-5 comment1.py

---
+++ Program 2-5 comment1.py
@@ -0,0 +1,5 @@
+#This program displays a person's
+#name and address
+print('Namie Nameson')      #This is and endline comment.
+print('123 Donut Drive')    #This is a street address.
+print('Bumfuck, Nowhere')   #This is a municipality and county adress.
