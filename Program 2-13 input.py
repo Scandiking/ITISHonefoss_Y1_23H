b673b15042df9b58a4f1cc899b5c9abb6d3c81d2
martian94 <57848767+Scandiking@users.noreply.github.com>
2023-08-29 15:40:06 +0200

    Create Program 2-13 input.py

---
+++ Program 2-13 input.py
@@ -0,0 +1,21 @@
+#Get the user's name, age and income.
+name = input('What is your name, dude? ')                #Notice the space.
+age = int(input('Dude, how old are you? '))              #Notice the int for telling we want the user to put in what python will treat like a number and not text. Also notice double paranthesis.
+income = float(input('How much moolah cha aquiring? '))  #float as opposed to int is used for decimal numbers
+
+#Display the tea (data)
+print('Here is the data you entered:')
+print('Name:', name)
+print('Age:', age)
+print('Income:', income)
+
+#Output
+
+#What is your name? Put in name (Press enter)
+#What is your age? Put in age (Press enter)
+#What is your income? Put in number point number
+
+#Here is the data you entered:
+#Name: Name You Put
+#Age: Age you put
+#Income: Income you put
