79fae4566c3ac583897bf0099173c94b398ee94c
martian94 <57848767+Scandiking@users.noreply.github.com>
2023-08-29 16:16:27 +0200

    Create Program 2-20 f_string_no_formatting.py

---
+++ Program 2-20 f_string_no_formatting.py
@@ -0,0 +1,6 @@
+#This program demonstrates how a floating-point
+#number is displayed with no formatting.
+amount_due = 5000.0
+monthly_payment = amount_due / 12.0
+print(f'THe monthly payment is {monthly_payment}.')    #Too many decimals? Check out program 2-21.
+
