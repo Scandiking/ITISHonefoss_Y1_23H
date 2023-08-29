f26387498fdc704e9f8c514fe445f90c73d9a3c9
Scandiking <57848767+Scandiking@users.noreply.github.com>
2023-08-29 14:54:58 +0200

    Create Program 2-6 comment2.py
    End line comments

---
+++ Program 2-6 comment2.py
@@ -0,0 +1,7 @@
+#Program 2-6, comment2.py
+print('Kate Austen')            #Display the name.
+print('123 Full Circle Drive')  #Display the address.
+print('Asheville, NC 28899')    #Display the city, state and ZIP.
+
+#Comments on line 2, 3 and 4 are known as end-line comments. Guess why. Yes, this is because they are at the end of the line.
+#Remember to add comments, especially for larger and more complex programs.
