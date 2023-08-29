6366c787078090c89977dbc5f0483f9ec469acab
Scandiking <57848767+Scandiking@users.noreply.github.com>
2023-08-29 15:06:23 +0200

    Create Program 2-9 variable_demo3.py
    Shows printed variable on one line

---
+++ Program 2-9 variable_demo3.py
@@ -0,0 +1,9 @@
+#This program demonstrates a variable, 
+#using the print function on one line 
+#rather than the two used in the former program.
+
+room = 503
+print('I am staying in room number', room)
+
+#Output
+#I am staying in room number 503
